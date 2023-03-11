a2a79307f4096cb7f2007f4a78fa7a42273b6417
Tom Haynes <tomhaynes94@gmail.com>
2023-03-11 10:41:55 +0000

    added state to update username and age inputs and added a console log to ensure that values were submitted by the onClick prop of the Button component

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Card from "../UI/Card";
 import Button from "../UI/Button";
@@ -6,19 +6,51 @@ import classes from "./AddUser.module.css";
 
 const AddUser = (props) => {
 
+    const [enteredUserName, setEnteredUserName] = useState('');
+    const [enteredAge, setEnteredAge] = useState('');
+
     const addUserHandler = (event) => {
         event.preventDefault();
+        console.log(enteredUserName, enteredAge);
+    };
+
+    const userNameChangeHandler = (event) => {
+        setEnteredUserName(event.target.value);
+    };
+
+    const ageChangeHandler = (event) => {
+        setEnteredAge(event.target.value);
     };
 
 return (
-    <Card className={classes.input}>
-        <form onSubmit={ addUserHandler }>
-            <label htmlFor="username">Username</label>
-            <input type="text" id="username" />
-            <label htmlFor="age">Age (Years)</label>
-            <input type="number" id="age" />
+    <Card 
+       className={ classes.input }
+    >
+        <form 
+           onSubmit={ addUserHandler }
+        >
+            <label 
+               htmlFor="username"
+            >
+                Username
+            </label>
+            <input 
+               type="text" 
+               id="username" 
+               onChange={ userNameChangeHandler }
+            />
+            <label 
+               htmlFor="age"
+            >
+                Age (Years)
+            </label>
+            <input 
+               type="number" 
+               id="age" 
+               onChange={ ageChangeHandler }
+            />
             <Button 
-                type="submit"
+               type="submit"
             >
                 Add User
             </Button>
