1d968ae2e74c954a1d306471997d19ef69965bc9
Tom Haynes <tomhaynes94@gmail.com>
2023-03-11 10:07:02 +0000

    added css modules for AddUser and Card components. To use styling with 'className' prop on the Card component, this was dynamically added to the <div> class in the Card JSX

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,5 +1,8 @@
 import React from "react";
 
+import Card from "../UI/Card";
+import classes from "./AddUser.module.css";
+
 const AddUser = (props) => {
 
     const addUserHandler = (event) => {
@@ -7,17 +10,19 @@ const AddUser = (props) => {
     };
 
 return (
-    <form onSubmit={ addUserHandler }>
-        <label htmlFor="username">Username</label>
-        <input type="text" id="username" />
-        <label htmlFor="age">Age (Years)</label>
-        <input type="number" id="age" />
-        <button 
-            type="submit"
-        >
-            Add User
-        </button>
-    </form>
+    <Card className={classes.input}>
+        <form onSubmit={ addUserHandler }>
+            <label htmlFor="username">Username</label>
+            <input type="text" id="username" />
+            <label htmlFor="age">Age (Years)</label>
+            <input type="number" id="age" />
+            <button 
+                type="submit"
+            >
+                Add User
+            </button>
+        </form>
+    </Card>
     );
 };
 
