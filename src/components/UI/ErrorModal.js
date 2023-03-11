0f0b9f00c61299c846837cda49651e111d6c4494
Tom Haynes <tomhaynes94@gmail.com>
2023-03-11 15:54:27 +0000

    Using the ErrorModal component to use portals in order to manipulate the positioning of the code within the DOM

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -1,25 +1,46 @@
 import React from "react";
+import ReactDOM from 'react-dom';
 
 import Card from "./Card";
 import Button from "./Button";
 import classes from "../UI/ErrorModal.module.css";
 
+const Backdrop = (props) => {
+  return <div className={classes.backdrop} onClick={props.onDismissError} />
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <Card className={classes.modal}>
+      <header className={classes.header}>
+        <h2>{props.errorTitle}</h2>
+      </header>
+      <div className={classes.content}>
+        <p>{props.errorMessage}</p>
+      </div>
+      <footer className={classes.actions}>
+        <Button onClick={props.onDismissError}>Dismiss</Button>
+      </footer>
+    </Card>
+  );
+};
+
 const ErrorModal = (props) => {
     return (
-      <div>
-        <div className={classes.backdrop} onClick={props.onDismissError} />
-        <Card className={classes.modal}>
-          <header className={classes.header}>
-            <h2>{props.errorTitle}</h2>
-          </header>
-          <div className={classes.content}>
-            <p>{props.errorMessage}</p>
-          </div>
-          <footer className={classes.actions}>
-            <Button onClick={props.onDismissError}>Dismiss</Button>
-          </footer>
-        </Card>
-      </div>
+      <>
+        {ReactDOM.createPortal(
+          <Backdrop onDismissError={props.onDismissError} />,
+          document.getElementById("backdrop-root")
+        )}
+        {ReactDOM.createPortal(
+          <ModalOverlay
+            errorTitle={props.errorTitle}
+            errorMessage={props.errorMessage}
+            onDismissError={props.onDismissError}
+          />,
+          document.getElementById('overlay-root')
+        )}
+      </>
     );
 };
 
